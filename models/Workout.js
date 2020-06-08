139e668fd849ccc7b36d9dd0d14948b0c715fab6
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-06-08 14:14:11 -0400

    developed models and server

---
+++ models/Workout.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: false
+        },
+        weight: {
+            type: Number,
+            required: false
+        },
+        reps: {
+            type: Number,
+            required: false
+        },
+        sets: {
+            type: Number,
+            required: false
+        },
+        distance: {
+            type: Number,
+            required: false
+        }
+    }]
+
+})
+
+const Workout = mongoose.model('Workout', workoutSchema)
+
+module.exports = Workout
